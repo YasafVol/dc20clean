53cd1fbfccb6e5e325db4f4a6653b7d2cd4275cb
guyzi1 <guyzinger@gmail.com>
2025-08-28 11:25:30 +0300

    class selection ui changes

---
+++ src/routes/character-creation/styles/CharacterCreationBG.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const CharacterCreationBG = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: url('/src/assets/BlackBG.jpg') center center/cover no-repeat fixed;
+  z-index: -1;
+`;
